a384c8477f29abd33732896e6923041937bcf12b
xianzicai <xianzhiyong763@gmail.com>
2024-10-16 15:18:56 +0800

    箭头控件开发

---
+++ Zicai.CaiNiaoPostStation/FrmNavigation.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace Zicai.CaiNiaoPostStation
+{
+    partial class FrmNavigation
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.uArrow1 = new Zicai.CaiNiaoPostStation.UControls.UArrow();
+            this.SuspendLayout();
+            // 
+            // uArrow1
+            // 
+            this.uArrow1.ArrowColor = System.Drawing.Color.LightSteelBlue;
+            this.uArrow1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.uArrow1.BorderColor = System.Drawing.Color.Aqua;
+            this.uArrow1.Direction = Zicai.CaiNiaoPostStation.UControls.UArrow.ArrowDirection.Down;
+            this.uArrow1.Location = new System.Drawing.Point(295, 89);
+            this.uArrow1.Name = "uArrow1";
+            this.uArrow1.Size = new System.Drawing.Size(53, 96);
+            this.uArrow1.TabIndex = 0;
+            // 
+            // FrmNavigation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1150, 683);
+            this.Controls.Add(this.uArrow1);
+            this.Name = "FrmNavigation";
+            this.Text = "FrmNavigation";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private UControls.UArrow uArrow1;
+    }
+}
---
+++ Zicai.CaiNiaoPostStation/UControls/UArrow.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Zicai.CaiNiaoPostStation.UControls
+{
+    public partial class UArrow : UserControl
+    {
+        public UArrow()
+        {
+            InitializeComponent();
+
+            // 控件样式标志设置
+            SetStyle(ControlStyles.AllPaintingInWmPaint, true);
+            SetStyle(ControlStyles.DoubleBuffer, true);
+            SetStyle(ControlStyles.ResizeRedraw, true);
+            SetStyle(ControlStyles.SupportsTransparentBackColor, true);
+            SetStyle(ControlStyles.UserPaint, true);
+            this.AutoScaleMode = AutoScaleMode.Inherit;
+            this.SizeChanged += UArrow_SizeChanged;
+            this.Size = new System.Drawing.Size(100, 50);
+        }
+
+        // 要绘制的路径
+        GraphicsPath path;
+
+        private Color arrowColor = Color.Blue;
+        // 箭头颜色
+        [Description("箭头颜色"), Category("自定义")]
+        public Color ArrowColor
+        {
+            get { return arrowColor; }
+            set
+            {
+                arrowColor = value;
+                Invalidate(); // 重绘
+            }
+        }
+
+        private Color? borderColor = null;
+        // 箭头边框颜色
+        [Description("箭头边框颜色"), Category("自定义")]
+        public Color? BorderColor
+        {
+            get { return borderColor; }
+            set
+            {
+                borderColor = value;
+                Invalidate(); // 重绘
+            }
+        }
+
+        private ArrowDirection direction;
+        // 箭头方向
+        [Description("箭头方向"), Category("自定义")]
+        public ArrowDirection Direction
+        {
+            get { return direction; }
+            set
+            {
+                direction = value;
+                // 重置路径
+                DrawArrowPath();
+                Invalidate(); // 重绘
+            }
+        }
+
+        /// <summary>
+        /// 控件大小改变时触发
+        /// </summary>
+        private void UArrow_SizeChanged(object sender, EventArgs e)
+        {
+            // 重置箭头路径（形状的区域）
+            DrawArrowPath();
+        }
+
+        /// <summary>
+        /// 生成形状路径
+        /// </summary>
+        private void DrawArrowPath()
+        {
+            Point[] points = null; // 点集合
+            switch (direction)
+            {
+                case ArrowDirection.Left:
+                    points = new Point[]
+                    {
+                        new Point(0, Height / 2),
+                        new Point(Width / 3, 0),
+                        new Point(Width / 3, Height / 4),
+                        new Point(Width - 1, Height / 4),
+                        new Point(Width - 1, Height - Height / 4),
+                        new Point(Width / 3, Height - Height / 4),
+                        new Point(Width / 3, Height),
+                       new Point(0, Height / 2)
+                    };
+                    break;
+                case ArrowDirection.Right:
+                    points = new Point[]
+                    {
+                        new Point(0, Height / 4),
+                        new Point(Width - Width / 3, Height / 4),
+                        new Point(Width - Width / 3, 0),
+                        new Point(Width - 1, Height / 2),
+                        new Point(Width - Width / 3, Height),
+                        new Point(Width - Width / 3, Height - Height / 4),
+                        new Point(0, Height - Height / 4),
+                        new Point(0, Height / 4)
+                    };
+                    break;
+                case ArrowDirection.Up:
+                    points = new Point[]
+                    {
+                       new Point(this.Width / 2, 0),
+                       new Point(this.Width, Height / 3),
+                       new Point(this.Width - this.Width / 4, Height /3),
+                       new Point(this.Width - this.Width / 4, this.Height - 1),
+                       new Point(this.Width / 4, this.Height - 1),
+                       new Point(this.Width / 4, Height / 3),
+                       new Point(0, Height / 3),
+                       new Point(this.Width / 2, 0)
+                    };
+                    break;
+                case ArrowDirection.Down:
+                    points = new Point[]
+                    {
+                       new Point(this.Width - this.Width / 4, 0),
+                       new Point(this.Width - this.Width / 4, this.Height - Height / 3),
+                       new Point(this.Width, this.Height - Height / 3),
+                       new Point(this.Width / 2, this.Height - 1),
+                       new Point(0, this.Height - Height / 3),
+                       new Point(this.Width / 4, this.Height - Height / 3),
+                       new Point(this.Width / 4, 0),
+                       new Point(this.Width - this.Width / 4, 0)
+                    };
+                    break;
+            }
+            path = new GraphicsPath();
+            path.AddLines(points);
+            path.CloseAllFigures();
+        }
+
+        /// <summary>
+        /// 绘图
+        /// </summary>
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            var g = e.Graphics;
+            g.SmoothingMode = SmoothingMode.AntiAlias;
+            g.FillPath(new SolidBrush(ArrowColor), path);
+            if (BorderColor != null && BorderColor != Color.Empty)
+            {
+                g.DrawPath(new Pen(new SolidBrush(borderColor.Value), 2), path);
+            }
+        }
+
+        /// <summary>
+        /// 箭头方向
+        /// </summary>
+        public enum ArrowDirection
+        {
+            Left,
+            Right,
+            Up,
+            Down
+        }
+    }
+}
