acbb9e7230165eb141ebf6fe38f65e87797f8776
xianzicai <xianzhiyong763@gmail.com>
2024-10-16 15:59:22 +0800

    系统导航页布局开发

--- Zicai.CaiNiaoPostStation/FrmNavigation.Designer.cs
+++ Zicai.CaiNiaoPostStation/FrmNavigation.Designer.cs
@@ -28,34 +28,219 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnStation = new System.Windows.Forms.Button();
+            this.btnEmployee = new System.Windows.Forms.Button();
+            this.btnSelves = new System.Windows.Forms.Button();
+            this.btnExpDistribution = new System.Windows.Forms.Button();
+            this.btnExpSelfPick = new System.Windows.Forms.Button();
+            this.btnExpStatistics = new System.Windows.Forms.Button();
+            this.btnExpress = new System.Windows.Forms.Button();
+            this.uArrow6 = new Zicai.CaiNiaoPostStation.UControls.UArrow();
+            this.uArrow5 = new Zicai.CaiNiaoPostStation.UControls.UArrow();
+            this.uArrow4 = new Zicai.CaiNiaoPostStation.UControls.UArrow();
+            this.uArrow3 = new Zicai.CaiNiaoPostStation.UControls.UArrow();
+            this.uArrow2 = new Zicai.CaiNiaoPostStation.UControls.UArrow();
             this.uArrow1 = new Zicai.CaiNiaoPostStation.UControls.UArrow();
             this.SuspendLayout();
             // 
+            // btnStation
+            // 
+            this.btnStation.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btnStation.FlatAppearance.BorderSize = 0;
+            this.btnStation.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnStation.Font = new System.Drawing.Font("楷体", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnStation.ForeColor = System.Drawing.Color.White;
+            this.btnStation.Location = new System.Drawing.Point(545, 93);
+            this.btnStation.Name = "btnStation";
+            this.btnStation.Size = new System.Drawing.Size(229, 82);
+            this.btnStation.TabIndex = 0;
+            this.btnStation.Text = "站点管理";
+            this.btnStation.UseVisualStyleBackColor = false;
+            // 
+            // btnEmployee
+            // 
+            this.btnEmployee.BackColor = System.Drawing.Color.Green;
+            this.btnEmployee.FlatAppearance.BorderSize = 0;
+            this.btnEmployee.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEmployee.Font = new System.Drawing.Font("楷体", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnEmployee.ForeColor = System.Drawing.Color.White;
+            this.btnEmployee.Location = new System.Drawing.Point(125, 93);
+            this.btnEmployee.Name = "btnEmployee";
+            this.btnEmployee.Size = new System.Drawing.Size(229, 82);
+            this.btnEmployee.TabIndex = 1;
+            this.btnEmployee.Text = "员工信息管理";
+            this.btnEmployee.UseVisualStyleBackColor = false;
+            // 
+            // btnSelves
+            // 
+            this.btnSelves.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.btnSelves.FlatAppearance.BorderSize = 0;
+            this.btnSelves.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSelves.Font = new System.Drawing.Font("楷体", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnSelves.ForeColor = System.Drawing.Color.White;
+            this.btnSelves.Location = new System.Drawing.Point(965, 93);
+            this.btnSelves.Name = "btnSelves";
+            this.btnSelves.Size = new System.Drawing.Size(229, 82);
+            this.btnSelves.TabIndex = 2;
+            this.btnSelves.Text = "货架管理";
+            this.btnSelves.UseVisualStyleBackColor = false;
+            // 
+            // btnExpDistribution
+            // 
+            this.btnExpDistribution.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.btnExpDistribution.FlatAppearance.BorderSize = 0;
+            this.btnExpDistribution.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExpDistribution.Font = new System.Drawing.Font("楷体", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnExpDistribution.ForeColor = System.Drawing.Color.White;
+            this.btnExpDistribution.Location = new System.Drawing.Point(545, 565);
+            this.btnExpDistribution.Name = "btnExpDistribution";
+            this.btnExpDistribution.Size = new System.Drawing.Size(229, 82);
+            this.btnExpDistribution.TabIndex = 3;
+            this.btnExpDistribution.Text = "快递派送";
+            this.btnExpDistribution.UseVisualStyleBackColor = false;
+            // 
+            // btnExpSelfPick
+            // 
+            this.btnExpSelfPick.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btnExpSelfPick.FlatAppearance.BorderSize = 0;
+            this.btnExpSelfPick.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExpSelfPick.Font = new System.Drawing.Font("楷体", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnExpSelfPick.ForeColor = System.Drawing.Color.White;
+            this.btnExpSelfPick.Location = new System.Drawing.Point(125, 329);
+            this.btnExpSelfPick.Name = "btnExpSelfPick";
+            this.btnExpSelfPick.Size = new System.Drawing.Size(229, 82);
+            this.btnExpSelfPick.TabIndex = 4;
+            this.btnExpSelfPick.Text = "快递自提";
+            this.btnExpSelfPick.UseVisualStyleBackColor = false;
+            // 
+            // btnExpStatistics
+            // 
+            this.btnExpStatistics.BackColor = System.Drawing.Color.Silver;
+            this.btnExpStatistics.FlatAppearance.BorderSize = 0;
+            this.btnExpStatistics.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExpStatistics.Font = new System.Drawing.Font("楷体", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnExpStatistics.ForeColor = System.Drawing.Color.White;
+            this.btnExpStatistics.Location = new System.Drawing.Point(965, 329);
+            this.btnExpStatistics.Name = "btnExpStatistics";
+            this.btnExpStatistics.Size = new System.Drawing.Size(229, 82);
+            this.btnExpStatistics.TabIndex = 5;
+            this.btnExpStatistics.Text = "快递统计";
+            this.btnExpStatistics.UseVisualStyleBackColor = false;
+            // 
+            // btnExpress
+            // 
+            this.btnExpress.BackColor = System.Drawing.Color.Blue;
+            this.btnExpress.FlatAppearance.BorderSize = 0;
+            this.btnExpress.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExpress.Font = new System.Drawing.Font("楷体", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnExpress.ForeColor = System.Drawing.Color.White;
+            this.btnExpress.Location = new System.Drawing.Point(545, 329);
+            this.btnExpress.Name = "btnExpress";
+            this.btnExpress.Size = new System.Drawing.Size(229, 82);
+            this.btnExpress.TabIndex = 6;
+            this.btnExpress.Text = "快递信息管理";
+            this.btnExpress.UseVisualStyleBackColor = false;
+            // 
+            // uArrow6
+            // 
+            this.uArrow6.ArrowColor = System.Drawing.Color.Teal;
+            this.uArrow6.BorderColor = System.Drawing.Color.DarkSlateBlue;
+            this.uArrow6.Direction = Zicai.CaiNiaoPostStation.UControls.UArrow.ArrowDirection.Down;
+            this.uArrow6.Location = new System.Drawing.Point(628, 181);
+            this.uArrow6.Name = "uArrow6";
+            this.uArrow6.Size = new System.Drawing.Size(59, 142);
+            this.uArrow6.TabIndex = 12;
+            // 
+            // uArrow5
+            // 
+            this.uArrow5.ArrowColor = System.Drawing.Color.White;
+            this.uArrow5.BorderColor = System.Drawing.Color.DarkSlateBlue;
+            this.uArrow5.Direction = Zicai.CaiNiaoPostStation.UControls.UArrow.ArrowDirection.Down;
+            this.uArrow5.Location = new System.Drawing.Point(628, 417);
+            this.uArrow5.Name = "uArrow5";
+            this.uArrow5.Size = new System.Drawing.Size(59, 142);
+            this.uArrow5.TabIndex = 11;
+            // 
+            // uArrow4
+            // 
+            this.uArrow4.ArrowColor = System.Drawing.Color.White;
+            this.uArrow4.BorderColor = System.Drawing.Color.DarkSlateBlue;
+            this.uArrow4.Direction = Zicai.CaiNiaoPostStation.UControls.UArrow.ArrowDirection.Right;
+            this.uArrow4.Location = new System.Drawing.Point(780, 347);
+            this.uArrow4.Name = "uArrow4";
+            this.uArrow4.Size = new System.Drawing.Size(179, 50);
+            this.uArrow4.TabIndex = 10;
+            // 
+            // uArrow3
+            // 
+            this.uArrow3.ArrowColor = System.Drawing.Color.White;
+            this.uArrow3.BorderColor = System.Drawing.Color.DarkSlateBlue;
+            this.uArrow3.Direction = Zicai.CaiNiaoPostStation.UControls.UArrow.ArrowDirection.Right;
+            this.uArrow3.Location = new System.Drawing.Point(780, 110);
+            this.uArrow3.Name = "uArrow3";
+            this.uArrow3.Size = new System.Drawing.Size(179, 50);
+            this.uArrow3.TabIndex = 9;
+            // 
+            // uArrow2
+            // 
+            this.uArrow2.ArrowColor = System.Drawing.Color.White;
+            this.uArrow2.BorderColor = System.Drawing.Color.DarkSlateBlue;
+            this.uArrow2.Direction = Zicai.CaiNiaoPostStation.UControls.UArrow.ArrowDirection.Left;
+            this.uArrow2.Location = new System.Drawing.Point(360, 347);
+            this.uArrow2.Name = "uArrow2";
+            this.uArrow2.Size = new System.Drawing.Size(179, 50);
+            this.uArrow2.TabIndex = 8;
+            // 
             // uArrow1
             // 
-            this.uArrow1.ArrowColor = System.Drawing.Color.LightSteelBlue;
-            this.uArrow1.BackColor = System.Drawing.SystemColors.ButtonFace;
-            this.uArrow1.BorderColor = System.Drawing.Color.Aqua;
-            this.uArrow1.Direction = Zicai.CaiNiaoPostStation.UControls.UArrow.ArrowDirection.Down;
-            this.uArrow1.Location = new System.Drawing.Point(295, 89);
+            this.uArrow1.ArrowColor = System.Drawing.Color.White;
+            this.uArrow1.BorderColor = System.Drawing.Color.DarkSlateBlue;
+            this.uArrow1.Direction = Zicai.CaiNiaoPostStation.UControls.UArrow.ArrowDirection.Left;
+            this.uArrow1.Location = new System.Drawing.Point(360, 110);
             this.uArrow1.Name = "uArrow1";
-            this.uArrow1.Size = new System.Drawing.Size(53, 96);
-            this.uArrow1.TabIndex = 0;
+            this.uArrow1.Size = new System.Drawing.Size(179, 50);
+            this.uArrow1.TabIndex = 7;
             // 
             // FrmNavigation
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1150, 683);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(163)))), ((int)(((byte)(233)))));
+            this.ClientSize = new System.Drawing.Size(1351, 754);
+            this.Controls.Add(this.uArrow6);
+            this.Controls.Add(this.uArrow5);
+            this.Controls.Add(this.uArrow4);
+            this.Controls.Add(this.uArrow3);
+            this.Controls.Add(this.uArrow2);
             this.Controls.Add(this.uArrow1);
+            this.Controls.Add(this.btnExpress);
+            this.Controls.Add(this.btnExpStatistics);
+            this.Controls.Add(this.btnExpSelfPick);
+            this.Controls.Add(this.btnExpDistribution);
+            this.Controls.Add(this.btnSelves);
+            this.Controls.Add(this.btnEmployee);
+            this.Controls.Add(this.btnStation);
             this.Name = "FrmNavigation";
-            this.Text = "FrmNavigation";
+            this.ShowIcon = false;
+            this.Text = " ";
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
+        private System.Windows.Forms.Button btnStation;
+        private System.Windows.Forms.Button btnEmployee;
+        private System.Windows.Forms.Button btnSelves;
+        private System.Windows.Forms.Button btnExpDistribution;
+        private System.Windows.Forms.Button btnExpSelfPick;
+        private System.Windows.Forms.Button btnExpStatistics;
+        private System.Windows.Forms.Button btnExpress;
         private UControls.UArrow uArrow1;
+        private UControls.UArrow uArrow2;
+        private UControls.UArrow uArrow3;
+        private UControls.UArrow uArrow4;
+        private UControls.UArrow uArrow5;
+        private UControls.UArrow uArrow6;
     }
 }
