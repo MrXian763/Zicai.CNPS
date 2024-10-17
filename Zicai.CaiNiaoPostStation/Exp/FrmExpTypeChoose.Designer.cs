c83cd3934d1c45f23bfacfba89e4eaf48a2ea3cd
xianzicai <xianzhiyong763@gmail.com>
2024-10-17 16:38:20 +0800

    快递类别选择页布局

---
+++ Zicai.CaiNiaoPostStation/Exp/FrmExpTypeChoose.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace Zicai.CaiNiaoPostStation.Exp
+{
+    partial class FrmExpTypeChoose
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label11 = new System.Windows.Forms.Label();
+            this.txtExpType = new System.Windows.Forms.TextBox();
+            this.chkExpTypes = new System.Windows.Forms.CheckedListBox();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.lblErrMsg = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label11.ForeColor = System.Drawing.Color.AliceBlue;
+            this.label11.Location = new System.Drawing.Point(42, 41);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(42, 22);
+            this.label11.TabIndex = 23;
+            this.label11.Text = "类别";
+            // 
+            // txtExpType
+            // 
+            this.txtExpType.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txtExpType.Location = new System.Drawing.Point(90, 39);
+            this.txtExpType.Name = "txtExpType";
+            this.txtExpType.Size = new System.Drawing.Size(486, 26);
+            this.txtExpType.TabIndex = 46;
+            // 
+            // chkExpTypes
+            // 
+            this.chkExpTypes.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.chkExpTypes.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.chkExpTypes.FormattingEnabled = true;
+            this.chkExpTypes.Items.AddRange(new object[] {
+            "省内",
+            "省外",
+            "大体积"});
+            this.chkExpTypes.Location = new System.Drawing.Point(46, 92);
+            this.chkExpTypes.Name = "chkExpTypes";
+            this.chkExpTypes.Size = new System.Drawing.Size(530, 319);
+            this.chkExpTypes.TabIndex = 47;
+            // 
+            // btnOk
+            // 
+            this.btnOk.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnOk.BackColor = System.Drawing.Color.Orange;
+            this.btnOk.FlatAppearance.BorderSize = 0;
+            this.btnOk.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnOk.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnOk.ForeColor = System.Drawing.Color.White;
+            this.btnOk.Location = new System.Drawing.Point(165, 441);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(86, 42);
+            this.btnOk.TabIndex = 61;
+            this.btnOk.Text = "确定";
+            this.btnOk.UseVisualStyleBackColor = false;
+            // 
+            // lblErrMsg
+            // 
+            this.lblErrMsg.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblErrMsg.AutoSize = true;
+            this.lblErrMsg.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lblErrMsg.ForeColor = System.Drawing.Color.Red;
+            this.lblErrMsg.Location = new System.Drawing.Point(347, 451);
+            this.lblErrMsg.Name = "lblErrMsg";
+            this.lblErrMsg.Size = new System.Drawing.Size(90, 21);
+            this.lblErrMsg.TabIndex = 63;
+            this.lblErrMsg.Text = "请选择类别";
+            this.lblErrMsg.Visible = false;
+            // 
+            // FrmExpTypeChoose
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(106)))), ((int)(((byte)(162)))), ((int)(((byte)(223)))));
+            this.ClientSize = new System.Drawing.Size(655, 512);
+            this.Controls.Add(this.lblErrMsg);
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.chkExpTypes);
+            this.Controls.Add(this.txtExpType);
+            this.Controls.Add(this.label11);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "FrmExpTypeChoose";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "快递类别选择";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox txtExpType;
+        private System.Windows.Forms.CheckedListBox chkExpTypes;
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.Label lblErrMsg;
+    }
+}
---
+++ Zicai.CaiNiaoPostStation/Exp/FrmExpTypeChoose.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Zicai.CaiNiaoPostStation.Exp
+{
+    public partial class FrmExpTypeChoose : Form
+    {
+        public FrmExpTypeChoose()
+        {
+            InitializeComponent();
+        }
+    }
+}
