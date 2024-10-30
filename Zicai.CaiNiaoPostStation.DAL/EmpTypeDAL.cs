bbe80c5e4508b4be2abbf0ce1df273dd7e9e114d
xianzicai <xianzhiyong763@gmail.com>
2024-10-30 09:43:37 +0800

    员工类别数据操作层开发

---
+++ Zicai.CaiNiaoPostStation.DAL/EmpTypeDAL.cs
@@ -0,0 +1,46 @@
+﻿using Helper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZiCai.CainiaoPostStation.DAL.Base;
+using ZiCai.CainiaoPostStation.Models;
+
+namespace Zicai.CaiNiaoPostStation.DAL
+{
+    public class EmpTypeDAL : BaseDAL<EmpTypeInfo>
+    {
+        /// <summary>
+        /// 获取所有员工类别
+        /// </summary>
+        /// <param name="isDeleted">1-已删除数据；0-未删除数据</param>
+        /// <returns></returns>
+        public DataTable GetEmpTypeList(int isDeleted)
+        {
+            string sql = CreateSql.CreateSelectSql<EmpTypeInfo>("", "IsDeleted=" + isDeleted, "");
+            return GetDt(sql, 1);
+        }
+
+        /// <summary>
+        /// 获取员工类别列表（绑定下拉框）
+        /// </summary>
+        /// <returns></returns>
+        public List<EmpTypeInfo> GetCboEmpTypes()
+        {
+            return GetAllModelList("EmpTypeId,EmpTypeName", "", 0);
+        }
+
+        /// <summary>
+        /// 保存员工类别信息
+        /// </summary>
+        /// <param name="dt">员工类别信息</param>
+        /// <returns></returns>
+        public bool SaveEmpTypeInfos(DataTable dt)
+        {
+            string sql = CreateSql.CreateSelectSql<EmpTypeInfo>("", "", "");
+            return SqlHelper.ExecuteDataTable(dt, sql) > 0;
+        }
+    }
+}
