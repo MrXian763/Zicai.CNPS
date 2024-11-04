6b5591a6104c74c48847fc24769929e20f71b5d9
xianzicai <xianzhiyong763@gmail.com>
2024-11-04 20:09:04 +0800

    快递自提数据操作层开发

---
+++ Zicai.CaiNiaoPostStation.DAL/ExpSelfPickDAL.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZiCai.CainiaoPostStation.DAL.Base;
+using ZiCai.CainiaoPostStation.Models;
+
+namespace Zicai.CaiNiaoPostStation.DAL
+{
+    public class ExpSelfPickDAL : BaseDAL<ExpSelfPickInfo>
+    {
+        /// <summary>
+        /// 获取今日的总自提快递数
+        /// </summary>
+        /// <returns></returns>
+        public int GetTodaySelfCount()
+        {
+            string sql = "select count(1) from ExpSelfPickInfos where left(convert(varchar,InsertTime,120),10)=left(convert(varchar,getdate(),120),10)";
+            return SelectAndReIntValue(sql);
+        }
+
+        /// <summary>
+        /// 获取指定的快递的自提信息
+        /// </summary>
+        /// <param name="expId">快递ID</param>
+        /// <returns></returns>
+        public ExpSelfPickInfo GetSelfPickInfo(int expId)
+        {
+            return GetModel("ExpId=" + expId + " and IsDeleted=0", "");
+        }
+    }
+}
